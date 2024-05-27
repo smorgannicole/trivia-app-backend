37454422482a7b0691414f7a738de30d7c78da12
smorgannicole <morgann.smithh@yahoo.com>
2024-05-27 09:29:21 -0700

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+categories = ['Sports', 'Entertainment', 'Science', 'History']
+categories.each do |category|
+  TriviaCategory.create(name: category)
+end
+
+TriviaCategory.all.each do |category|
+  10.times do
+    category.trivia_questions.create(
+      question: "Sample question for #{category.name}",
+      correct_answer: "Correct answer",
+      incorrect_answers: ["Wrong answer 1", "Wrong answer 2", "Wrong answer 3"]
+    )
+  end
+end
